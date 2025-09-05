13cac2e2b0b055434f03bfbdbdc157916d2b189b
Naveen <info.dnc199303@gmail.com>
2025-09-05 19:54:18 +0530

    Create individual Vercel serverless functions for loans and payments

---
+++ api/payments.py
@@ -0,0 +1,45 @@
+from supabase import create_client, Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase: Client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_ANON_KEY"))
+
+def handler(event, context):
+    try:
+        if event['httpMethod'] == 'GET':
+            response = supabase.table("payments").select("*").execute()
+            return {
+                'statusCode': 200,
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Methods': '*'
+                },
+                'body': str(response.data)
+            }
+        elif event['httpMethod'] == 'POST':
+            import json
+            body = json.loads(event['body'])
+            response = supabase.table("payments").insert(body).execute()
+            return {
+                'statusCode': 200,
+                'headers': {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Headers': '*',
+                    'Access-Control-Allow-Methods': '*'
+                },
+                'body': str([response.data[0]])
+            }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': str({'error': str(e)})
+        }
