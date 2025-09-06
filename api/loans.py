d65c9533d085476b610a287d8dabbfca428386d8
Naveen <info.dnc199303@gmail.com>
2025-09-06 22:04:40 +0530

    Change to Vercel Python runtime API format with request parameter

--- api/loans.py
+++ api/loans.py
@@ -1,15 +1,9 @@
-class Handler:
-    def __init__(self):
-        pass
-
-    def __call__(self, event, context):
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            'body': '{"message": "Test function working"}'
-        }
-
-handler = Handler()
+def handler(request):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': '{"message": "Test function working"}'
+    }
