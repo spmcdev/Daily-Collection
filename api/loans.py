bf113b2c1bacdcc5f82607c3bf969a2e8e524bef
Naveen <info.dnc199303@gmail.com>
2025-09-06 22:01:40 +0530

    Change to class-based handler to fix Vercel issubclass error

--- api/loans.py
+++ api/loans.py
@@ -1,11 +1,15 @@
-from http.server import BaseHTTPRequestHandler
+class Handler:
+    def __init__(self):
+        pass
 
-def handler(event, context):
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        'body': '{"message": "Test function working"}'
-    }
+    def __call__(self, event, context):
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': '{"message": "Test function working"}'
+        }
+
+handler = Handler()
